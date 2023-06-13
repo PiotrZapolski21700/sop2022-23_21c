8227830b9eb2541e80d85558ab2e72b7a74748d7
s27100-pj <116441361+PiotrZapolski21700@users.noreply.github.com>
2023-06-13 14:27:18 +0200

    dodano zwalnianie pamieci

--- s27100-PiotrZapolski/zajecia4/zad_dom_2.c
+++ s27100-PiotrZapolski/zajecia4/zad_dom_2.c
@@ -43,6 +43,20 @@ void displayBackward(struct Node* tail) {
     printf("\n");
 }
 
+// Funkcja do zwalniania pamięci zaalokowanej dla listy
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    struct Node* nextNode;
+
+    while (current != NULL) {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+
+    printf("Memory successfully freed.\n");
+}
+
 // Funkcja główna
 int main() {
     struct Node* head = NULL; // Inicjalizacja głowy listy jako NULL
@@ -63,6 +77,9 @@ int main() {
     }
     tail = current;
     displayBackward(tail);
+
+    // Zwalnianie pamięci zaalokowanej dla listy
+    freeList(head);
 
     return 0;
 }
