342069784b65483d6f26821bc28708c87f1073db
s27100-pj <116441361+PiotrZapolski21700@users.noreply.github.com>
2023-06-13 14:30:38 +0200

    poprawiono zrownoleglenie

--- s27100-PiotrZapolski/zajecia5/zad3.c
+++ s27100-PiotrZapolski/zajecia5/zad3.c
@@ -13,23 +13,37 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int fd[2];
+    if (pipe(fd) == -1) {
+        printf("Failed to create a pipe\n");
+        return 1;
+    }
+
     pid_t child_pid = fork();
 
     if (child_pid == -1) {
         printf("%d Failed to create a child!\n", getpid());
     } else if (child_pid == 0) {
+        close(fd[1]); // Child nie pisze do potoku, więc zamykamy zapis.
         printf("Child process PID: %d\tPPID: %d\n", getpid(), getppid());
         for (index_i = 1; index_i < arg_value; index_i += 2) {
             printf("Number: %d\n", index_i);
         }
         printf("Child process PID: %d has finished\n", getpid());
+        // Czytanie z potoku i wydrukowanie wyniku
+        read(fd[0], &result, sizeof(result));
+        printf("Parent PID: %d - Result: %d\n", getppid(), result);
+        close(fd[0]);
     } else {
-        wait(NULL);
         for (index_j = 0; index_j < arg_value; index_j++) {
             result += index_j;
         }
-        printf("Parent PID: %d - Result: %d\n", getpid(), result);
+        close(fd[0]); // Parent nie czyta z potoku, więc zamykamy odczyt.
+        write(fd[1], &result, sizeof(result));
+        close(fd[1]);
+        wait(NULL); // Czekamy na zakończenie procesu potomnego.
     }
 
     return 0;
 }
+
